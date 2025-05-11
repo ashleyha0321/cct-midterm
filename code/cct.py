e77b7eaeceae17409a324c85060b11bc2c57c193
ashleyha <ah@class-docker>
2025-05-11 19:55:12 +0000

    representing plant_knowledge.csv

--- code/cct.py
+++ code/cct.py
@@ -1,2 +1,18 @@
 import numpy as np
-import csv
+import pandas as pd
+import os
+
+# loading plant_knowledge.csv
+def load_plant_knowledge():
+    base_dir = os.path.dirname(os.path.abspath(__file__))  
+    filepath = os.path.join(base_dir, '../data/plant_knowledge.csv')
+    df = pd.read_csv(filepath)
+    data = df.drop(columns=['Informant']).values
+    return data
+
+X = load_plant_knowledge()
+N, M = X.shape
+
+#print(X)
+
+
